5373f928cd236736a625371e5a83ea268f4be8df
miautawn <miautawn@gmail.com>
2021-04-07 21:36:49 +0300

    removed a stray 'file.py'

--- file.py
+++
@@ -1,9 +0,0 @@
-import re
-import locale
-locale.setlocale(locale.LC_ALL)
-
-text = "SSAš, valgiau ir dabar 'nebevalgauųųų \n fdsfdsa'"
-
-text = text.lower()
-text = re.sub(r'[^a-ž\n ]', '', text)
-print(text)
